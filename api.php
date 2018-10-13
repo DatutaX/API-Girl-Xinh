e0c4561e188b9bb31b9828de08a924e756375dbd
thanhdatneu <thanhdat@live.com>
2018-10-13 22:30:46 +0700

    Thanh đổi mysqli

--- api.php
+++ api.php
@@ -4,20 +4,20 @@ $hp2k1_user = "xxx";
 $hp2k1_pass = "xxx";
 $hp2k1_dbname = "Bot";
 
-$connection = mysql_connect($hp2k1_host,$hp2k1_user,$hp2k1_pass);
+$connection = mysqli_connect($hp2k1_host,$hp2k1_user,$hp2k1_pass,$hp2k1_dbname);
 if (!$connection)
   {
-  die('Could not connect: ' . mysql_error());
+  die('Could not connect: ' . mysqli_error());
   }
-mysql_select_db($hp2k1_dbname) or die(mysql_error());
-mysql_query("SET NAMES utf8");
+
+
 $sql = 'SELECT id, url FROM girl';
 $sqlrandom ='SELECT url FROM girl order by RAND()';
-$hp2k1_go = mysql_query( $sqlrandom, $connection );
+$hp2k1_go = mysqli_query($connection, $sqlrandom);
 if(!$hp2k1_go ) {
-      die('Could not get data: ' . mysql_error());
+      die('Could not get data: ' . mysqli_error());
    }
-$row = mysql_fetch_array($hp2k1_go, MYSQL_NUM);
+$row = mysqli_fetch_array($hp2k1_go, MYSQLI_NUM);
 echo '{ "messages": [ { "attachment": { "type": "image", "payload": { "url": "';
 echo $row[0];
 echo '" } } },{ "text": "<3", "quick_replies": [ { "title":"Xem tiếp!!!", "block_names":["HP2k1", "girl"] } ] } ] }';
